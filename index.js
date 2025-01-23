7dbd94be296672e27bc7c044d1b93eed9c302190
Norbert <wadjanorbert@gmail.com>
2025-01-23 10:15:43 +0000

    les petits revision de js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("helo");
+//Fichier de revision en javascript
+//simple affichage
+
+console.log("hello Man");
+console.warn("YO YUJI %s","Sa dit quoi");
+
+
+//declaration de variable
+let amount =6;
+let price =4.99;
+let x = null;
+let name ="Tommy";
+let found = false;
+
+
+//affichage des variables declarer
+name ="ITADORI";
+console.log("la persone: ",x," ou ", name,found,);
+
+//etude des chaine de charactere
+let single = "where my bandit hat?";
+let double = "le manga le plus chaud du moment pout moi est solo leveling";
+console.log(double.length);
+single="norbert";
+console.log("le nombre de charactere de la premiere phrase est : ",single.length);
+
